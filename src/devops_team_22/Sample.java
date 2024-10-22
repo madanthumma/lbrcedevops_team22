1a42a9d1f84be08e522604308c8e888f5eaac81b
AdmiN <AdmiN@ITWS000>
2024-10-22 10:26:17 +0530

    commit

---
+++ src/devops_team_22/Sample.java
@@ -0,0 +1,14 @@
+package devops_team_22;
+
+public class Sample {
+
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
